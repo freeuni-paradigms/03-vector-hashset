7d2e5098320489450cfcd8cfc30175dfa1bc1a67
Luka Samkharadze <samkharadzeluke@gmail.com>
2020-10-13 15:34:32 +0400

    finished setup

--- setup.sh
+++ setup.sh
@@ -1,2 +1,6 @@
-chmod u+x thesaurus-lookup-* # TODO 
+chmod u+x thesaurus-lookup-*
 chmod u+x zip.sh
+wget https://github.com/freeuni-paradigms/assn-3-vector-hashset-data/archive/master.zip
+unzip master.zip
+mv assn-3-vector-hashset-data-master data
+rm master.zip
